583705a5727e7e5f73f65c209231872e4b0434f6
Jamil Abreu <abreu.jamil@gmail.com>
2013-07-19 23:06:57 -0400

    figaro rake

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Commoncast::Application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -42,7 +43,7 @@ Commoncast::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
