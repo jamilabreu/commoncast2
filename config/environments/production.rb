9c36cca19c220eae8b0aaed80e9e256c212b2c2a
Jamil Abreu <abreu.jamil@gmail.com>
2013-07-21 21:54:34 -0400

    precompile js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Commoncast::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( active_admin.css active_admin.js )
+  config.assets.precompile += %w( active_admin.css active_admin.js select2.js posts/new.js users/new.js users/edit.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
