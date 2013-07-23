09fa14875e2f5415f3168663c61d66eec0df0b2c
Jamil Abreu <abreu.jamil@gmail.com>
2013-07-23 12:02:13 -0400

    more redesign stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,12 @@ class UsersController < ApplicationController
   def index
     if params[:recent]
       @posts = Post.joins(:communities).where(community_posts: { community_id: current_user.community_ids }).select('posts.*, COUNT(distinct communities.id) AS community_count').having('COUNT(distinct communities.id) >= 2').group('posts.id').order(created_at: :desc)
+    elsif params[:rewind]
+      @posts = Post.joins(:communities).where("posts.created_at <= ?", DateTime.now - 1.week).where(approved: true, community_posts: {community_id: current_user.community_ids}).select('posts.*, COUNT(distinct communities.id) AS community_count').having('COUNT(distinct communities.id) >= 2').group('posts.id').order(created_at: :desc)
     else
       @posts = Post.joins(:communities).where(approved: true, communities: {id: current_user.community_ids}).select('posts.*, COUNT(distinct communities.id) AS community_count').having('COUNT(distinct communities.id) >= 2').group('posts.id').order('community_count DESC')
       @pending = Post.joins(:communities).where(approved: false, communities: { id: current_user.community_ids }).select('posts.*, COUNT(distinct communities.id) AS community_count').having('COUNT(distinct communities.id) >= 2').group('posts.id').order('community_count DESC')
+      redirect_to root_path(recent: true) if @posts.blank? && @pending.blank?
     end
   end
 end
