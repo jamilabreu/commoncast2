c8de7d4001c94fd582bd2347ce3e427a6c097cbf
Jamil Abreu <abreu.jamil@gmail.com>
2013-07-24 18:16:24 -0400

    UserMailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,18 @@ Commoncast::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Mailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => ENV["GMAIL_EMAIL"],
+    :password => ENV["GMAIL_PASSWORD"]
+  }
 end
